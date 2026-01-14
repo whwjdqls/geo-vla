b2729f4dd2072c5216580df58f5563791ba84275
whwjdqls <whwjdqls99@yonsei.ac.kr>
2026-01-14 20:00:49 +0900

    added pi05_libero_full_ft_openvla_libero

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1474,27 +1474,27 @@ _CONFIGS = [
         # Turn off EMA for LoRA finetuning.
         ema_decay=None,
     ),
-    # TrainConfig(
-    #     name="pi05_libero_full_",
-    #     model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False),
-    #     data=LeRobotLiberoDataConfig(
-    #         repo_id="physical-intelligence/libero",
-    #         base_config=DataConfig(prompt_from_task=True),
-    #         extra_delta_transform=False,
-    #     ),
-    #     batch_size=256,
-    #     lr_schedule=_optimizer.CosineDecaySchedule(
-    #         warmup_steps=10_000,
-    #         peak_lr=5e-5,
-    #         decay_steps=1_000_000,
-    #         decay_lr=5e-5,
-    #     ),
-    #     optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
-    #     ema_decay=0.999,
-    #     weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
-    #     pytorch_weight_path="/path/to/your/pytorch_weight_path",
-    #     num_train_steps=30_000,
-    # ),
+    TrainConfig(
+        name="pi05_libero_full_ft_openvla_libero",
+        model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False),
+        data=LeRobotLiberoDataConfig(
+            repo_id="whwjdqls99/libero_hdfr_lerobot_track_datasets",
+            base_config=DataConfig(prompt_from_task=True),
+            extra_delta_transform=False,
+        ),
+        batch_size=256,
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            warmup_steps=10_000,
+            peak_lr=5e-5,
+            decay_steps=1_000_000,
+            decay_lr=5e-5,
+        ),
+        optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
+        ema_decay=0.999,
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+        pytorch_weight_path="/path/to/your/pytorch_weight_path",
+        num_train_steps=30_000,
+    ),
     TrainConfig(
         name="pi05_libero",
         model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False),
